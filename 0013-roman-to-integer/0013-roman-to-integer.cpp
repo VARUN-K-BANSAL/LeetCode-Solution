c942dc7c51b874dcd96414a2e0dc28ba59e6f4bc
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2022-10-16 22:56:09 +0530

    Time: 17 ms (62.12%), Space: 6.1 MB (65.37%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int value(char r)
+        {
+            if (r == 'I')
+                return 1;
+            if (r == 'V')
+                return 5;
+            if (r == 'X')
+                return 10;
+            if (r == 'L')
+                return 50;
+            if (r == 'C')
+                return 100;
+            if (r == 'D')
+                return 500;
+            if (r == 'M')
+                return 1000;
+            return 0;
+        }
+    
+    int romanToInt(string s) {
+        int numerals=0;
+        for(int i=0;i<s.length();i++){
+            int a=value(s[i]),b;
+            if(i <= (s.length()-1)){
+                b=value(s[i+1]);
+            if(a<b){
+                numerals+=(b-a);
+                i++;
+            }
+            else{
+                numerals+=a;
+            }
+            }
+            else{
+                numerals+=a;
+            }
+        }
+        return numerals;
+    }
+};

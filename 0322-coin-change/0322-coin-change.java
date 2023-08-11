d971cb939d836d1d1d0fbac029d67bc3a5da8e79
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2023-08-12 01:01:17 +0530

    Time: 16 ms (79.09%), Space: 43.3 MB (61.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -37,14 +37,31 @@ class Solution {
         return dp[amount] = min;
     }
     
+    public int solveDp2(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for(int i=1; i<=amount; i++) {
+            for(int j=0; j<coins.length; j++) {
+                if(i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);                                           
+            }
+        }
+        return dp[amount];
+    }
+    
     public int coinChange(int[] coins, int amount) {
         // using recursion
         //int ans = solve(coins, amount);
         
         // optimised approach using top-down dp
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        int ans = solveMem(coins, amount, dp);
+        // int[] dp = new int[amount + 1];
+        // Arrays.fill(dp, -1);
+        // int ans = solveMem(coins, amount, dp);
+        
+        // Optimised approach suign bottom-up approach
+        int ans = solveDp2(coins, amount);
         
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }

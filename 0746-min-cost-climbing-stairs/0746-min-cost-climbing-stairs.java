a24bfae6703a75eacd4edd5d869e6178815b8b4e
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2023-08-12 00:33:03 +0530

    Time: 0 ms (100.00%), Space: 43 MB (61.33%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -16,6 +16,17 @@ class Solution {
         return dp[n] = Math.min(solveDp(cost, n-1, dp), solveDp(cost, n-2, dp)) + cost[n];
     }
     
+    public int solveDp2(int[] cost, int n) {
+        int[] dp = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2; i<n; i++)
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+        
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+    
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         
@@ -23,10 +34,13 @@ class Solution {
         // int ans = Math.min(solve(cost, n-1), solve(cost, n-2));
         
         // Optimised approach using top-down dp
-        int[] dp = new int[n+1];
-        for(int i=0; i<=n; i++)
-            dp[i] = -1;
-        int ans = Math.min(solveDp(cost, n-1, dp), solveDp(cost, n-2, dp));
+        // int[] dp = new int[n+1];
+        // for(int i=0; i<=n; i++)
+        //     dp[i] = -1;
+        // int ans = Math.min(solveDp(cost, n-1, dp), solveDp(cost, n-2, dp));
+        
+        // Optimised approach using bottom-up dp
+        int ans = solveDp2(cost, n);
         
         return ans;
     }

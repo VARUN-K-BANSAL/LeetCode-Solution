f805c62aaa4784a70136395a6744606fdef49958
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2023-08-12 10:22:27 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (69.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -20,14 +20,37 @@ class Solution {
         return dp[n] = Math.max(solveMem(nums, n-2, dp) + nums[n], solveMem(nums, n-1, dp));
     }
     
+    public int solveTab(int[] nums) {
+        int n = nums.length;
+        
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return nums[0];
+        if(n == 2)
+            return Math.max(nums[0], nums[1]);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for(int i=2; i<n; i++)
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        
+        return dp[n-1];
+    }
+    
     public int rob(int[] nums) {
         // using recursion will give TLE
         // int ans = solve(nums, nums.length - 1);
         
         // Using top-down dp approach
-        int[] dp = new int[nums.length + 1];
-        Arrays.fill(dp, -1);
-        int ans = solveMem(nums, nums.length - 1, dp);
+        // int[] dp = new int[nums.length + 1];
+        // Arrays.fill(dp, -1);
+        // int ans = solveMem(nums, nums.length - 1, dp);
+        
+        
+        // using bottom-up dp approach
+        int ans = solveTab(nums);
         
         
         return ans;

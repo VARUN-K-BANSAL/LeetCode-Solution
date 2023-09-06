3d3ead98a9b56ce010a1e0656b4d258f0d8b1068
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2023-09-06 16:23:40 +0530

    Time: 2 ms (96.29%), Space: 41 MB (15.65%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -3,22 +3,18 @@ import java.util.HashMap;
 class Solution {
 
     public boolean uniqueOccurrences(int[] arr) {
-
         int n = arr.length;
-
         HashMap<Integer, Integer> hm = new HashMap<>();
 
-        // Count the occurrences of each element in arr
         for (int i = 0; i < n; ++i) {
             hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
         }
 
         HashMap<Integer, Integer> hm2 = new HashMap<>();
 
-        // Count the occurrences of each frequency
         for (int freq : hm.values()) {
             if (hm2.containsKey(freq)) {
-                return false; // If a frequency occurs more than once, return false
+                return false;
             }
             hm2.put(freq, 1);
         }

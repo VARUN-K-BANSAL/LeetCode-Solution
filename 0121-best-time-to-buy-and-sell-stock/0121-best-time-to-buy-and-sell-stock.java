7a9d67698268268297f591c246e475cd58432c7b
Varun Kumar Bansal <73171376+VARUN-K-BANSAL@users.noreply.github.com>
2023-09-26 10:33:24 +0530

    Time: 3 ms (9.96%), Space: 61.2 MB (25.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[] suff = new int[n];
-        suff[n-1] = prices[n-1];
-        for(int i=n-2; i>=0; i--) {
-            if(prices[i] > suff[i + 1]) {
-                suff[i] = prices[i];
-            } else {
-                suff[i] = suff[i + 1];
-            }
-        }
+//         int[] suff = new int[n];
+//         suff[n-1] = prices[n-1];
+//         for(int i=n-2; i>=0; i--) {
+//             if(prices[i] > suff[i + 1]) {
+//                 suff[i] = prices[i];
+//             } else {
+//                 suff[i] = suff[i + 1];
+//             }
+//         }
         
+//         int ans = 0;
+//         for(int i=0; i<n; i++) {
+//             if(suff[i] - prices[i] > ans) {
+//                 ans = suff[i] - prices[i];
+//             }
+//         }
+        
+        
+//         2nd approach
         int ans = 0;
+        int min = Integer.MAX_VALUE;
         for(int i=0; i<n; i++) {
-            if(suff[i] - prices[i] > ans) {
-                ans = suff[i] - prices[i];
-            }
+            min = Math.min(min, prices[i]);
+            ans = Math.max(ans, prices[i] - min);
         }
         
         return ans;
